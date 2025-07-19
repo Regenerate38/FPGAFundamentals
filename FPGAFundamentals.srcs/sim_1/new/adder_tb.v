5c462a2b3f57781a0a685cb59707690bfaae6405
Regenerate38 <mukunda.adhikari@outlook.com>
2025-07-19 10:09:27 +0545

    Implemented 4bit Adder from previous 1b Adder using Structural Modelling

--- FPGAFundamentals.srcs/sim_1/new/adder_tb.v
+++ FPGAFundamentals.srcs/sim_1/new/adder_tb.v
@@ -2,24 +2,32 @@
 
 module adder_tb;
 
-    reg A, B, C_in;
-    wire S, C_out;
+    reg [3:0]A;
+    reg [3:0]B;
+    reg C_in;
+    wire [3:0]S;
+    wire C_out;
     
-    adder uut (
+    adder_4b uut (
         .A(A), .B(B) ,.C_in(C_in), .S (S), .C_out(C_out)
     );
     
-    initial begin
-      A = 0; B = 0; C_in = 0;
-        #20; A = 0; B = 0; C_in = 1;
-        #20; A = 0; B = 1; C_in = 0;
-        #20; A = 0; B = 1; C_in = 1;
-        #20; A = 1; B = 0; C_in = 0;
-        #20; A = 1; B = 0; C_in = 1;
-        #20; A = 1; B = 1; C_in = 0;
-        #20; A = 1; B = 1; C_in = 1;
-        #20;
-        $finish;
+ initial begin
+    A = 4'b0000; B = 4'b0000; C_in = 0; 
+    // Sum = 0000, Carry_out = 0
+    #20; A = 4'b0000; B = 4'b0001; C_in = 0; 
+    // Sum = 0001, Carry_out = 0
+    #20; A = 4'b0010; B = 4'b0011; C_in = 0; 
+    // Sum = 0101, Carry_out = 0
+    #20; A = 4'b0101; B = 4'b0110; C_in = 1; 
+    // Sum = 1100, Carry_out = 0
+    #20; A = 4'b1111; B = 4'b0001; C_in = 0; 
+    // Sum = 0000, Carry_out = 1
+    #20; A = 4'b1010; B = 4'b0101; C_in = 1; 
+    // Sum = 0000, Carry_out = 1
+    #20; A = 4'b1111; B = 4'b1111; C_in = 1; 
+    // Sum = 1111, Carry_out = 1
+    #20;
+    $finish;
     end
-    
 endmodule
--- FPGAFundamentals.srcs/sources_1/new/adder.v
+++ FPGAFundamentals.srcs/sources_1/new/adder.v
@@ -8,3 +8,32 @@ module adder(
     assign S = A ^ B ^ C_in;
     assign  C_out = A & B | A & C_in | B & C_in;
 endmodule
+
+
+module adder_4b(
+    input wire [3:0]A,
+    input wire [3:0]B,
+    input wire  C_in,
+    output wire [3:0]S,
+    input wire C_out
+    );
+    
+    wire [2:0] C_temp;
+   
+    adder adder0 (
+    .A(A[0]), .B(B[0]) ,.C_in(C_in), .S (S[0]), .C_out(C_temp[0])
+    );
+    
+    adder adder1 (
+    .A(A[1]), .B(B[1]) ,.C_in(C_temp[0]), .S (S[1]), .C_out(C_temp[1])
+    );
+    
+    adder adder2 (
+    .A(A[2]), .B(B[2]) ,.C_in(C_temp[1]), .S (S[2]), .C_out(C_temp[2])
+    );
+    
+    adder adder3 (
+    .A(A[3]), .B(B[3]) ,.C_in(C_temp[2]), .S (S[3]), .C_out(C_out)
+    );
+    
+endmodule
