e6850d9cef730461fe19af1bf9f31b69738bb64b
Regenerate38 <mukunda.adhikari@outlook.com>
2025-07-21 17:59:17 +0545

    Impl SIPO and PIPO Reg

--- FPGAFundamentals.srcs/sim_1/new/register_tb.v
+++ FPGAFundamentals.srcs/sim_1/new/register_tb.v
@@ -4,31 +4,33 @@ module register_tb;
     reg Din;
     reg CLK;
     wire Out;
+    wire Q3;
+    wire Q2;
+    wire Q1;
+    reg D2;
+    reg D1;
+    reg D0;
     
-    siso_register uut (
-        .Din(Din), .CLK(CLK), .Out(Out)
+//    siso_register uut (
+//        .Din(Din), .CLK(CLK), .Out(Out)
+//    );
+
+//    sipo_register uut (
+//        .Din(Din), .CLK(CLK), .Out(Out), .Q3(Q3), .Q2(Q2), .Q1(Q1)
+//    );
+
+    pipo_register uut (
+        .D3(Din), .D2(D2), .D1(D1), .D0(D0), .CLK(CLK), .Q3(Q3), .Q2(Q2), .Q1(Q1), .Out(Out)
     );
     initial CLK = 0;
     always #5 CLK=~ CLK;
     initial begin
      @(posedge CLK);
      Din = 1;
+     D2 = 0;
+     D1 = 1;
+     D0 = 1;
      @(posedge CLK);
-     Din = 0;
-     @(posedge CLK);
-     Din = 1;
-     @(posedge CLK);
-     Din = 1;
-     @(posedge CLK);
-     Din = 0;
-     @(posedge CLK);
-     Din = 0;
-     @(posedge CLK);
-     Din = 0;
-     @(posedge CLK);
-     Din = 0;
-     @(posedge CLK);
-          @(posedge CLK);
      $finish;
     end
 endmodule
--- FPGAFundamentals.srcs/sources_1/new/register.v
+++ FPGAFundamentals.srcs/sources_1/new/register.v
@@ -23,3 +23,63 @@ module siso_register(
       .D(Q1), .CLK(CLK), .Q(Out)
     );
 endmodule
+
+
+module sipo_register(
+    input wire CLK,
+    input wire Din,
+    output wire Out,
+    output wire Q3,
+    output wire Q2,
+    output wire Q1
+
+    );
+    
+    d_flipflop FF3 (
+      .D(Din), .CLK(CLK), .Q(Q3)
+    );
+
+    d_flipflop FF2 (
+      .D(Q3), .CLK(CLK), .Q(Q2)
+    );
+    
+    d_flipflop FF1 (
+      .D(Q2), .CLK(CLK), .Q(Q1)
+    );
+    
+    d_flipflop FF0 (
+      .D(Q1), .CLK(CLK), .Q(Out)
+    );
+endmodule
+
+module pipo_register(
+    input wire CLK,
+    input wire D3,
+    input wire D2,
+    input wire D1,
+    input wire D0,
+    output wire Out,
+    output wire Q3,
+    output wire Q2,
+    output wire Q1
+
+    );
+    
+    d_flipflop FF3 (
+      .D(D3), .CLK(CLK), .Q(Q3)
+    );
+
+    d_flipflop FF2 (
+      .D(D2), .CLK(CLK), .Q(Q2)
+    );
+    
+    d_flipflop FF1 (
+      .D(D1), .CLK(CLK), .Q(Q1)
+    );
+    
+    d_flipflop FF0 (
+      .D(D0), .CLK(CLK), .Q(Out)
+    );
+endmodule
+
+
